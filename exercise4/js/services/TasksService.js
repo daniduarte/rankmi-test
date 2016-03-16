57c49794d3ad10f2606457b0d4d3f26771614b8a
Daniel Duarte <dduarte@goplaceit.com>
2016-03-16 18:54:10 -0300

    corrección en datos

--- exercise4/js/services/TasksService.js
+++ exercise4/js/services/TasksService.js
@@ -2,7 +2,7 @@
 app.factory('TasksService', function(){
   var tasks = [
     { text: 'Comprar pan para la once', done: false, comments: ['Lorem ipsum 1', 'Lorem ipsum 2', 'Lorem ipsum 3'] },
-    { text: 'Limpiar el baño de visitas', done: true, coments: [] },
+    { text: 'Limpiar el baño de visitas', done: true, comments: [] },
     { text: 'Regar las plantas del patio de atrás', done: false, comments: ['Lorem ipsum 1'] },
     { text: 'Lavar la ropa', done: false, comments: ['Lorem ipsum 1', 'Lorem ipsum 2'] },
     { text: 'Pintar bodega chica', done: false, comments: [] }
