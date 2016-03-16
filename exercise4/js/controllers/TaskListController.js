455ae5e7f6842436cda73ed7b6e6dee69e087a9b
Daniel Duarte <dduarte@goplaceit.com>
2016-03-16 14:40:07 -0300

    separando lógica en diferentes archivos

---
+++ exercise4/js/controllers/TaskListController.js
@@ -0,0 +1,21 @@
+
+app.controller('TaskListController', function(){
+
+  var taskList = this;
+
+  taskList.tasks = [
+    { text: 'Comprar pan para la once', done: false },
+    { text: 'Limpiar el baño de visitas', done: true },
+    { text: 'Regar las plantas del patio de atrás', done: false },
+    { text: 'Lavar la ropa', done: false },
+    { text: 'Pintar bodega chica', done: false }
+  ];
+
+  taskList.addTodo = function(){
+    taskList.tasks.push({
+      text: 'Comprar leche para la semana',
+      done: false
+    });
+  };
+
+});
